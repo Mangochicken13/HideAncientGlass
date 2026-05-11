efdb41a04df2b16b7551a68c4dd2b246805d1796
Mangochicken <mangochkn@gmail.com>
2026-05-12 01:30:06 +0800

    forgot the annotation

--- src/NCardPatches.cs
+++ src/NCardPatches.cs
@@ -4,11 +4,14 @@ using MegaCrit.Sts2.Core.Nodes.Cards;
 
 namespace RevertAncientBorder;
 
-[HarmonyPatch(typeof(NCard), nameof(NCard.Reload))]
+[HarmonyPatch(typeof(NCard))]
 public static class NCardPatches
 {
+    [HarmonyPatch(nameof(NCard.Reload))]
+    [HarmonyPostfix]
     public static void HideGlassOverlay(NCard __instance)
     {
+        if (!__instance.IsNodeReady() || __instance == null) { return; }
         if (__instance.Model?.Rarity == CardRarity.Ancient)
         {
             __instance._ancientBorderGlassOverlay.Visible = false;
